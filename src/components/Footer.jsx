76fb139050bca3166187dbbf7854b8f290b3bf67
byllee <febrianisianipar524@gmail.com>
2025-05-16 11:07:53 +0700

    Styling footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../styles/Footer.css';
 
 const Footer = () => {
     return (
         <footer className="Footer">
             <p>&copy; 2025 Petalora. Semua hak dilindungi.</p>
+            <div className="footer-contact">
+                <p>Kontak: petalora@gmail.com | Telp : 0852-0000-1232</p>
+                <p>Alamat: Jl. Raya Petalora No. 123, Berlin, Indonesia</p>
+            </div> 
         </footer>
     );
 };
